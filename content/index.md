4978fed248d9a2a7c7400bac0ce08ff973d055c7
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2022-09-06 11:02:40 +0200

    FEATURE: add links

--- content/index.md
+++ content/index.md
@@ -1,6 +1,6 @@
-Title: DelphinePythonique
+Title: Accueil
 Date: 2022-09-06 10:20
 Category: Accueil
-save_as: index.html
+save_as: accueil.html
 
 Voici la page d'accueil de mon portfolio.
--- pelicanconf.py
+++ pelicanconf.py
@@ -16,16 +16,21 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+        ('Tryton', 'https://www.tryton.org/'),
+        ('Forum Tryton', 'https://discuss.tryton.org/'),
+      )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/delphine-lemire/'),
+          ('Github', 'https://github.com/DelphinePythonique'),
+         )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+DISPLAY_CATEGORIES_ON_MENU = False
+
+DISPLAY_PAGES_ON_MENU = False
